d224b8e9db1a38b9dd0914900c3aacbb6dac85db
RamziddinCoder <mrramziddindeveloper@gmail.com>
2024-05-23 18:30:32 +0500

    added

--- src/pages/teachers/Teachers.jsx
+++ src/pages/teachers/Teachers.jsx
@@ -235,7 +235,7 @@ function Teachers() {
               }
               type="number"
             />
-            <button disabled={loading}>{loading ? "Save" : "Create"}</button>
+            <button>{edit ? "Save" : "Create"}</button>
           </form>
         </div>
       </div>
